38887c2e9e0815f4cf908348c56a0dccd95b5cbf
Yuriy Brovko <ysb.ptc@gmail.com>
2022-01-30 18:22:39 +0200

    Прошел  все задачки

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@
 
 
 // ===== replace vs replacAll =====
-//  
+//
 // const newStr = str.replace(' ', '_');
 // const newStr = str.replace(/ /gi, '_');
 // const newStr = str.replaceAll(' ', '_');
@@ -154,14 +154,14 @@
 
 // 70 покажет 01:10
 // 450 покажет 07:30
-// 1441 покажет 24:01   
-const totalMinutes = 65; // 01:10
+// 1441 покажет 24:01
+// const totalMinutes = 65; // 01:10
 
-const hours = Math.floor(totalMinutes / 60);
-const minutes = totalMinutes % 60;
-console.log(hours);
-console.log(minutes);
+// const hours = Math.floor(totalMinutes / 60);
+// const minutes = totalMinutes % 60;
+// console.log(hours);
+// console.log(minutes);
 
-const doubleDigitHours = String(hours).padStart(2, 0);
-const doubleDigitMinutes = String(minutes).padStart(2, 0);
-console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
+// const doubleDigitHours = String(hours).padStart(2, 0);
+// const doubleDigitMinutes = String(minutes).padStart(2, 0);
+// console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
