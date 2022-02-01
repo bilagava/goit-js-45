a506fac4fd9877e042a0e16c54d8d7a581ac4652
Yuriy Brovko <ysb.ptc@gmail.com>
2022-02-01 19:45:02 +0200

    Тренировка

--- src/index.js
+++ src/index.js
@@ -165,3 +165,23 @@
 // const doubleDigitHours = String(hours).padStart(2, 0);
 // const doubleDigitMinutes = String(minutes).padStart(2, 0);
 // console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
+
+//----------------------------------------------------------
+// let item;
+// function nextInLine(arr, item) {
+//     arr.push(item);
+//     item = arr.shift();
+//     return item;
+// }
+// const testArr = [1, 2, 3, 4, 5];
+
+// console.log("Before: " + JSON.stringify(testArr));
+// console.log(nextInLine(testArr, 6));
+// console.log("After: " + JSON.stringify(testArr));
+
+// typeof 3;
+// typeof '3';
+// console.log(typeof '3');
+
+
+
